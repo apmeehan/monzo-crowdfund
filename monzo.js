de2622c280d7067aa983704cdb978581f4b81384
apmeehan <petermeehan@Peters-MBP.home>
2017-03-07 01:34:05 +0000

    1st draft

--- monzo.js
+++ monzo.js
@@ -1,20 +1,83 @@
 var PD = require("probability-distributions");
 
-var goal = 25000;
-var pledgeValues = [10,20,50,100,250,500,1000];
+var goal = 2500;
+var pledgeValues = [10,20,50,100];
 var maxPledgeValue = pledgeValues[pledgeValues.length - 1];
-var dataset = PD.sample(pledgeValues, 1000, true, [7,6,5,4,3,2,1]);
+var dataset = PD.sample(pledgeValues, 1000, true, [7,6,5,4]);
 
 var datasetTotal = dataset.reduce((a, b) => a + b, 0);
-var randomlyChosenPledge;
-while (datasetTotal >= goal ) {
-	randomlyChosenPledge = dataset.splice(
-    Math.floor(Math.random() * dataset.length), 1
-  )[0];
-	datasetTotal -= randomlyChosenPledge;
+
+/*  Convert array to object so each pledge has an ID */
+dataset = dataset.reduce(function(obj, currentValue, index) {
+    obj[index] = currentValue;
+    return obj;
+}, {});
+
+/*  Make copy of original dataset and total and randomly select pledges to
+    to remove so it now contains only chosen pledges */
+var chosenDatasetTotal = datasetTotal;
+var chosenDataset = {};
+for (var i in dataset) chosenDataset[i] = dataset[i]; // replace with Object.assign()?
+while (chosenDatasetTotal >= goal ) {
+  var ids = Object.keys(chosenDataset);
+	var randomId = ids[Math.floor(Math.random() * ids.length)];
+	chosenDatasetTotal -= chosenDataset[randomId];
+  delete chosenDataset[randomId];
+}
+/*  Update original dataset by removing chosen applicants, so they can't
+    be chosen again later if more applicants are required */
+for (var i in chosenDataset) delete dataset[i];
+
+console.log("\nGoal: " + goal);
+console.log("Total pledged: " + datasetTotal);
+console.log("Number of chosen pledgers: " + Object.keys(chosenDataset).length);
+console.log("Chosen pledges total: " + chosenDatasetTotal);
+
+
+
+/*  Remove failed pledges */
+var numberOfDroppedPledgers = 10;
+var subset = getRandomSubsetOfChosenApplicants(chosenDataset, numberOfDroppedPledgers);
+removeSubsetOfApplicants(chosenDataset, subset);
+
+console.log("\nOH FUCK, WE LOST " + numberOfDroppedPledgers + " PLEDGERS!");
+console.log("New number of chosen pledgers: " + Object.keys(chosenDataset).length);
+console.log("New chosen pledges total: " + chosenDatasetTotal);
+
+
+/*  Repopulate chosen pledges dataset */
+repopulateChosenDataset();
+
+console.log("\nNEW PLEDGERS SELECTED");
+console.log("New number of chosen pledgers: " + Object.keys(chosenDataset).length);
+console.log("New chosen pledges total: " + chosenDatasetTotal);
+
+
+
+function getRandomSubsetOfChosenApplicants(obj, numberOf) {
+  var subset = [];
+  var keys = Object.keys(obj);
+  for (var i = 0; i < numberOf; i++) {
+    var randomIndex = Math.floor(Math.random() * keys.length);
+    var randomKey = keys.splice(randomIndex, 1)[0];
+    subset.push(randomKey);
+  }
+  return subset;
 }
 
-console.log("Goal: " + goal);
-console.log("Total pledged: " + datasetTotal);
-console.log("Number of chosen pledgers: " + dataset.length);
-console.log("Chosen pledges total: " + datasetTotal);
+function removeSubsetOfApplicants(obj, arrayOfApplicantIds) {
+  for (i = 0; i < arrayOfApplicantIds.length; i++) {
+    chosenDatasetTotal -= obj[arrayOfApplicantIds[i]];
+    delete obj[arrayOfApplicantIds[i]];
+  }
+}
+
+function repopulateChosenDataset() {
+  while (chosenDatasetTotal <= goal - maxPledgeValue) {
+    var ids = Object.keys(dataset);
+  	var randomId = ids[Math.floor(Math.random() * ids.length)];
+  	chosenDatasetTotal += dataset[randomId];
+    chosenDataset[randomId] = dataset[randomId];
+    delete dataset[randomId];
+  }
+}
